6acd6c4baba23bff5b44e9f556eed4f5485f1f5b
Dmitry Mishchenko <d.mishchanka@godeltech.com>
2016-01-19 08:13:03 +0200

    Refactored to simple helloworld app

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('views'));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.use('/*', express.static('./views/pages/index.html'));
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+
+app.get('*', function(req, res) {
+  res.sendfile('./views/pages/index.html');
 });
 
 app.listen(app.get('port'), function() {
