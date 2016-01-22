0f6c00403d2ff5f3705fb298cf9e7bee7a893217
Dmitry Mishchenko <d.mishchanka@godeltech.com>
2016-01-22 10:22:30 +0200

    Restructured and renamed

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static('views'));
+
+app.use('/home', express.static('./public/index.html'));
+
+app.get('/api/*', function(req, res) {
+    console.log(req.originalUrl);
+    res.writeHead(302, {
+        'Location': 'http://habit-server.herokuapp.com' + req.originalUrl
+    });
+    res.end();
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
+
+
